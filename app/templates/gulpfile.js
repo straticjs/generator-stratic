231294e36dbed92b48fd1d02d485d7a7471d3c30
Alex Jordan <alex@strugee.net>
2016-05-26 17:00:19 -0700

    Make building Jade and blog posts work

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,10 +1,33 @@
+'use strict';
+
 var gulp = require('gulp');
+var jade = require('gulp-jade');
+var rename = require('gulp-rename');
+var parse = require('stratic-parse-header');
+var markdown = require('gulp-markdown');
+var straticToJson = require('stratic-post-to-json-data');
+var jadeTemplate = require('gulp-jade-template');
 
 gulp.task('build', ['build:html', 'build:css', 'build:js'], function() {
-	
+
 });
 
-gulp.task('build:html', [''], function() {
+gulp.task('build:html', function() {
+	return gulp.src('src/*.jade')
+	           .pipe(jade({ pretty: true }))
+	           .pipe(rename({ extname: '.html' }))
+	           .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build:blog:posts', function() {
+	return gulp.src('src/blog/*.md')
+	           .pipe(parse())
+	           .pipe(markdown())
+	           .pipe(straticToJson())
+	           .pipe(jadeTemplate('src/blog/post.jade'))
+	           .pipe(rename({ extname: '.html' }))
+	           .pipe(gulp.dest('dist/blog'));
+});
 
 });
 
