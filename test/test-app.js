f622d1b6f92bf1362449da78a7266aa8b7ea632c
Alex Jordan <alex@strugee.net>
2017-02-21 16:30:02 -0500

    Assert that templating actually works in the tests

--- test/test-app.js
+++ test/test-app.js
@@ -6,6 +6,21 @@ var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
 var os = require('os');
 
+var files = ['package.json',
+             '.editorconfig',
+             'package.json',
+             'gulpfile.js',
+             '.gitignore',
+             'src/index.jade',
+             'src/blog/post.jade',
+             'src/blog/index.jade',
+             'src/blog/hello-world.md',
+             'src/includes/layout.jade',
+             'src/includes/post.jade',
+             'src/scripts/main.js',
+             'src/styles/main.styl',
+             '.jshintrc'];
+
 describe('stratic:app', function () {
 	beforeEach(function () {
 		return helpers.run(path.join(__dirname, '../app'))
@@ -17,21 +32,12 @@ describe('stratic:app', function () {
 	});
 
 	it('creates files', function () {
-		assert.file([
-			'package.json',
-			'.editorconfig',
-			'package.json',
-			'gulpfile.js',
-			'.gitignore',
-			'src/index.jade',
-			'src/blog/post.jade',
-			'src/blog/index.jade',
-			'src/blog/hello-world.md',
-			'src/includes/layout.jade',
-			'src/includes/post.jade',
-			'src/scripts/main.js',
-			'src/styles/main.styl',
-			'.jshintrc'
-		]);
+		assert.file(files);
+	});
+
+	it('templates files properly', function () {
+		assert.noFileContent(files.map(function (filename) {
+			return [filename, '<%'];
+		}));
 	});
 });
