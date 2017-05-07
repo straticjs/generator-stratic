d7dfb2e8483545f905e37e45387de3e79d63cb3f
Alex Jordan <alex@strugee.net>
2017-05-07 00:39:11 -0700

    Create a .gitkeep file in src/images/
    According to yeoman/yeoman#1668 the Right Way™ to do this is to create
    an empty .gitkeep file, particularly because git doesn't track empty
    directories.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
-var mkdirp = require('mkdirp');
 
 var StraticGenerator = yeoman.extend({
 	initializing () {
@@ -95,8 +94,7 @@ var StraticGenerator = yeoman.extend({
 		},
 
 		images () {
-			/* TODO: don't do sync I/O here */
-			mkdirp.sync(path.join(this.destinationRoot(), 'src/images/'));
+			this.fs.write(this.destinationPath('src/images/.gitkeep'), '');
 		},
 
 		blogTemplates () {
